64153c56401b833ce6e00743c4b3025456c29bd6
Patrick <example@mail.com>
2019-01-06 10:27:03 +0100

    first commit

---
+++ EntitiyFrameworkDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntitiyFrameworkDemo
+{
+    class Program
+    {
+        public class Recipient
+        {
+            public string name { get; set; }
+            public string location { get; set; }
+        };
+
+        public class Paket
+        {
+            public Recipient recipient { get; set; }
+            public int weight { get; set; }
+        };
+
+        public class PostDbContext :DbContext
+        {
+            public PostDbContext() : base()
+            {
+                //Database.EnsureCreated();
+            }
+
+            public DbSet<Paket> Pakete { get; set; }
+            public DbSet<Recipient> Recipients { get; set; }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            using (var ctx = new PostDbContext())
+            {
+                var paket = new Paket() { weight = 20 };
+
+                ctx.Pakete.Add(paket);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
